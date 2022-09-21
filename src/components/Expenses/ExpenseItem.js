13969d74e27950c310f062a41861784937e423a1
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-09-21 10:47:25 +0200

    fixed ExpenseItem component, and delete state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
@@ -9,21 +9,13 @@ const ExpenseItem = (props) => {
   //   const expenseTitle = "Car Insurance";       <---- is not dynamic
   //   const expenseAmount = 294.67;
 
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log("cliccato");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
